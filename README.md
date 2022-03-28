269ea7492b90f0290708bc6d0622f243637dd35d
davi78 <davialfajr05@gmail.com>
2022-03-28 18:13:10 +0700

    Music

---
+++ README.md
@@ -0,0 +1,65 @@
+<h2 align="centre">Asuna Music Player 🎵</h2>
+
+### A bot that can play music on telegram group's voice call
+##
+<p align="center">
+  <img src="https://telegra.ph/file/d262cc6a47d7a709acecc.jpg">
+<b>Play from Title</b>
+</p>
+
+##
+
+<p align="center">
+  <img src="https://telegra.ph/file/63b1e16cabccff6b70b30.jpg">
+<b>Play from YT Url</b>
+</p>
+
+##
+
+<p align="center">
+  <img src="https://telegra.ph/file/69d584442148204d83223.jpg">
+<b>Play from Media File</b>
+</p>
+
+##
+
+<h3>Requirements 📝</h3>
+
+- FFmpeg
+- NodeJS [nodesource.com](https://nodesource.com/)
+- Python 3.7+
+- [PyTgCalls](https://github.com/pytgcalls/pytgcalls)
+
+### Commands 🛠
+#### For all in group
+- `/play` yt url - or reply to youtube url
+- `/play` - reply song file to play song
+- `/play <song name>` - play song you requested
+- `/song <song name>` - download songs you want quickly
+- `/search <query>` - search videos on youtube with details
+- `/video <query>` - download high quality video from youtube
+- `/lyrics <query>` - sends the complete lyrics of the song
+- `/glyrics <query>` - search song lyrics with title/artist name
+
+#### Admins only
+- `/pause` - pause song play
+- `/resume` - resume song play
+- `/skip` - play next song
+- `/end` - stop music play
+- `/userbotjoin` - add assistant to group
+- `/userbotleave` - let userbot leave your group
+- `/reload` - refresh adminlist
+
+### Deploy To Heroku</h4>
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/zYxDevs/AsunaSmartMusic)
+
+Use [Repl Link](http://string.projectdils.repl.run) to get pyrogram string session
+
+### Credits
+- [UserLazy](https://github.com/UserLazy): Dev
+- [zYxDevs](https://github.com/zYxDevs): Editor
+- [Laky](https://github.com/Laky-64) & [Andrew](https://github.com/AndrewLaneX): PyTgCalls
+- [Original Repo](https://github.com/suprojects/CallsMusic)
+- [Oda Nobunaga](https://t.me/OdaRobot): Bot
+- [Asuna Yuuki](https://t.me/YogaWaifuBot): Bot
---
+++ handlers/admins.py
@@ -0,0 +1,100 @@
+from asyncio.queues import QueueEmpty
+from config import que, BOT_USERNAME
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import sira
+import tgcalls
+from cache.admins import set
+from helpers.decorators import authorized_users_only, errors
+from helpers.channelmusic import get_chat_id
+from helpers.filters import command, other_filters
+from callsmusic import callsmusic
+from callsmusic.queues import queues
+
+
+@Client.on_message(command(["pause", f"pause@{BOT_USERNAME}"]) & other_filters)
+@errors
+@authorized_users_only
+async def pause(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
+        callsmusic.pytgcalls.active_calls[chat_id] == "paused"
+    ):
+        await message.reply_text("❗ No music played!")
+    else:
+        callsmusic.pytgcalls.pause_stream(chat_id)
+        await message.reply_text("▶️ Paused!")
+
+
+@Client.on_message(command(["resume", f"resume@{BOT_USERNAME}"]) & other_filters)
+@errors
+@authorized_users_only
+async def resume(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    if (chat_id not in callsmusic.pytgcalls.active_calls) or (
+        callsmusic.pytgcalls.active_calls[chat_id] == "playing"
+    ):
+        await message.reply_text("❗ Nothing paused!")
+    else:
+        callsmusic.pytgcalls.resume_stream(chat_id)
+        await message.reply_text("⏸ Resumed!")
+
+
+@Client.on_message(command(["end", f"end@{BOT_USERNAME}"]) & other_filters)
+@errors
+@authorized_users_only
+async def stop(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    if chat_id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("❗ Nothing song played!")
+    else:
+        try:
+           callsmusic.queues.clear(message.chat.id)
+        except QueueEmpty:
+           pass
+
+        callsmusic.pytgcalls.leave_group_call(message.chat.id)
+        await message.reply_text("❌ Stop the Song!")
+
+
+@Client.on_message(command(["skip", f"skip@{BOT_USERNAME}"]) & other_filters)
+@errors
+@authorized_users_only
+async def skip(_, message: Message):
+    global que
+    if message.chat.id not in callsmusic.pytgcalls.active_calls:
+        await message.reply_text("❗ Nothing is playing to skip!")
+    else:
+        callsmusic.queues.task_done(message.chat.id)
+
+        if callsmusic.queues.is_empty(message.chat.id):
+            callsmusic.pytgcalls.leave_group_call(message.chat.id)
+        else:
+            callsmusic.pytgcalls.change_stream(
+                message.chat.id,
+                callsmusic.queues.get(message.chat.id)["file"]
+            )
+
+    qeue = que.get(message.chat.id)
+    if qeue:
+        skip = qeue.pop(0)
+    if not qeue:
+        return
+    await message.reply_text(f"Skipped **{skip[0]}**\nNow Playing **{qeue[0][0]}**")
+
+
+@Client.on_message(filters.command(["reload", f"reload@{BOT_USERNAME}"]))
+@errors
+@authorized_users_only
+async def admincache(client, message: Message):
+    set(
+        message.chat.id,
+        (
+            member.user
+            for member in await message.chat.get_members(
+                filter="administrators"
+            )
+        ),
+    )
+
+    await message.reply_text("❇️ Admin list is updated")
